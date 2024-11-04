c0ec5279ddcc2ae4ff8d785dd8dc39591547e31b
wagner1067 <barra-wag@hotmail.com>
2024-11-03 21:56:56 -0300

    Feat: Create Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { GlobalStyle, lighTheme } from './styles/';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={lighTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
