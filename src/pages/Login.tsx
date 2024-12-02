880903522c68f5b471fc1a810b80890118806dcc
wagner1067 <barra-wag@hotmail.com>
2024-12-02 16:33:03 -0300

    Feat: Login page requests

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,68 @@
+import { ChangeEvent, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import Cookies from "js-cookie";
+
+//HOOKS
+import { useFormValidation, usePost } from "@/hooks";
+
+//UTILS
+import { jwtExpirationDateConverter, pxToRem } from "@/utils";
+
+//COMPONENTS
 import { BannerImage, FormComponent, Logo, StyledH1, StyledP, } from "@/components";
-import { pxToRem } from "@/utils";
 
 // MUI
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 
-
+//TYPES
+import { DecodedJWT, MessageProps, LoginData, LoginPostData } from "@/types";
 
 function Login() {
+    const navigate = useNavigate()
+    const inputs = [
+        { type: 'email', placeholder: 'Email' },
+        { type: 'password', placeholder: 'Senha' }
+    ]
+    const { data, loading, error, postData } = usePost<LoginData, LoginPostData>(
+        'login'
+    )
+    const { formValues, formValid, handleChange } = useFormValidation(inputs)
+
+    const handleMessage = (): MessageProps => {
+        if (!error) return { msg: '', type: 'success' }
+        switch (error) {
+            case 401: return {
+                msg: 'Email e e/ou senha inválidos',
+                type: 'error'
+            }
+            default: return {
+                msg: 'Não foi possível realizar a operação. Entre em contato com nosso suporte.',
+                type: 'error'
+            }
+        }
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        await postData({
+            email: String(formValues[0]),
+            password: String(formValues[1])
+        })
+    }
+
+    useEffect(() => {
+        if (data?.jwt_token) {
+            const decoded: DecodedJWT = jwtDecode(data?.jwt_token)
+            Cookies.set('Authorization', data?.jwt_token, {
+                expires: jwtExpirationDateConverter(decoded.exp),
+                secure: true,
+            })
+        }
+        if (Cookies.get('Authorization')) navigate('/home')
+    }, [data, navigate])
 
     return (
         <>
@@ -26,22 +80,23 @@ function Login() {
                                 <StyledH1>Bem-vindo</StyledH1>
                                 <StyledP>Digite sua senha e email para logar</StyledP>
                             </Box>
-                            <FormComponent inputs={[
-                                { type: 'email', placeholder: 'Email', disabled: true },
-                                { type: 'password', placeholder: 'Senha' }
-                            ]}
+                            <FormComponent inputs={inputs.map((input, index) => ({
+                                type: input.type,
+                                placeholder: input.placeholder,
+                                value: formValues[index] || '',
+                                onChange: (e: ChangeEvent<HTMLInputElement>) => handleChange(index, (e.target as HTMLInputElement).value)
+                            }))}
                                 buttons={[
                                     {
                                         className: 'primary',
+                                        disabled: !formValid || loading,
                                         type: 'submit',
-                                        children: 'Login',
-                                        disabled: true,
+                                        onClick: handleSubmit,
+                                        children: loading ? 'Aguarde...' : 'Login'
                                     }
                                 ]}
-                                message={{
-                                    msg: 'Sucesso!!!',
-                                    type: 'success'
-                                }} />
+                                message={handleMessage()}
+                            />
                         </Container>
                     </Grid>
                     <Grid
