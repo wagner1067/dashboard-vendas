2aa0afc385c742733bd587424315520b016b403a
wagner1067 <barra-wag@hotmail.com>
2024-12-02 17:00:28 -0300

    Feat: Logout flow

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import { DefaultTheme } from "styled-components";
+import { pxToRem } from "@/utils";
 
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
 body, html{
@@ -13,4 +14,12 @@ h1,h2,p,ul,li,figure{
     margin: 0;
     padding: 0;
 }
-`;
+
+.mb-1 {
+    margin-bottom: ${pxToRem(16)};
+}
+
+.mb-2 {
+    margin-bottom: ${pxToRem(32)};
+}
+`
