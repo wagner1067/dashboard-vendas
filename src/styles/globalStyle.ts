942ef8553fbabd52b505b5b70a99fe983eab72e2
wagner1067 <barra-wag@hotmail.com>
2024-12-04 23:02:28 -0300

    Feat: Home Page Request

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -22,4 +22,25 @@ h1,h2,p,ul,li,figure{
 .mb-2 {
     margin-bottom: ${pxToRem(32)};
 }
+
+.skeleton-loading {
+    animation: skeletonLoading 2s infinite alternate;
+}
+
+@keyframes skeletonLoading {
+    from{
+        background-color: ${(props) => props.theme.appSkeletonFrom};
+    }
+    to{
+         background-color: ${(props) => props.theme.appSkeletonTo};
+    }
+}
+
+.skeleton-loading-mh-1{
+    min-height: ${pxToRem(175)};
+}
+
+.skeleton-loading-mh-2{
+    min-height: ${pxToRem(400)};
+}
 `
