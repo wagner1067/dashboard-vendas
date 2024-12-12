45e7bc46a1614c97ddf9d2074039f2c86f96be29
wagner1067 <barra-wag@hotmail.com>
2024-12-12 00:17:07 -0300

    Feat: Leads Requests

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/`
 })
 
-export const usePost = <T, P>(endpoint: string) => {
+export const usePost = <T, P>(endpoint: string, withAuth?: boolean) => {
     const [data, setData] = useState<T | null>(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState<number | null>(null)
@@ -20,14 +20,20 @@ export const usePost = <T, P>(endpoint: string) => {
         setError(null)
 
         try {
+            const headers = withAuth ? {
+                Authorization: `Bearer ${Cookies.get('Authorization')}`,
+                'Content-Type': 'application/json',
+                ...config?.headers
+            }
+                : {
+                    'Content-Type': 'application/json',
+                    ...config?.headers
+                }
             const response = await axiosInstance({
                 url: endpoint,
                 method: 'POST',
                 data: postData,
-                headers: {
-                    'Content-Type': 'application/json',
-                    ...config?.headers
-                },
+                headers: headers,
                 ...config
             })
             setData(response.data)
@@ -62,7 +68,7 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
             })
             setData(response.data)
         } catch (e: any) {
-            setError(e.responde.status ?? 500)
+            setError(e.response.status ?? 500)
         } finally {
             setLoading(false)
         }
@@ -99,7 +105,7 @@ export const usePut = <T>(endpoint: string) => {
             })
             setData(response.data)
         } catch (e: any) {
-            setError(e.responde.status ?? 500)
+            setError(e.response.status ?? 500)
         } finally {
             setLoading(false)
         }
--- src/pages/Leads.tsx
+++ src/pages/Leads.tsx
@@ -1,11 +1,164 @@
-import { Header } from "@/components"
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ChangeEvent, useState, useEffect } from "react"
+
+//COMPONENTS
+import {
+    CardComponent,
+    CustomTable,
+    Header,
+    FormComponent,
+    StyledH2,
+    StyledButton,
+    StyledSpan,
+    StyledP
+} from "@/components"
+
+//MUI
+import { Container, Grid } from "@mui/material"
+
+//HOOK
+import { useFormValidation, useGet, usePost, useDelete } from "@/hooks"
+
+//TYPES
+import { InputProps, LeadsData, LeadsPostData, MessageProps } from "@/types"
 
 function Leads() {
+
+    //HOOK
+    const { data: createLeadsData,
+        loading: createLeadsLoading,
+        error: createLeadsError,
+        postData: createLeadsPostData
+    } = usePost<LeadsData, LeadsPostData>('leads/create', true)
+
+    const {
+        data: leadsData,
+        loading: leadsLoading,
+        error: leadsError,
+        getData: getLeads
+    } = useGet<LeadsData[]>('leads')
+
+    const { deleteData: leadsDeleteData, loading: leadsDeleteLoading } = useDelete('leads/delete')
+
+    //FORM
+    const inputs: InputProps[] = [
+        { name: 'name', type: 'text', placeholder: 'Nome', required: true },
+        { name: 'email', type: 'email', placeholder: 'Email', required: true },
+        { name: 'phone', type: 'tel', placeholder: 'Telefone', required: true },
+    ]
+    const { formValues, formValid, handleChange } = useFormValidation(inputs)
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        await createLeadsPostData({
+            name: String(formValues[0]),
+            email: String(formValues[1]),
+            phone: String(formValues[2])
+        })
+    }
+
+    const handleDelete = async (id: number) => {
+        if (
+            confirm('Tem certeza que deseja excluir seu lead? ')
+        ) {
+            try {
+                await leadsDeleteData({ params: { id: id } })
+                alert('Lead deletado com sucesso!')
+                getLeads()
+            } catch (e) {
+                alert('Não foi possível realizar a operação. Entre em contato com nosso suporte.')
+            }
+        }
+    }
+
+    const [createMessage, setCreateMessage] = useState<MessageProps>({
+        type: 'success',
+        msg: ''
+    })
+    const clearMessage = () => {
+        setTimeout(() => {
+            setCreateMessage({
+                type: 'success',
+                msg: ''
+            })
+        }, 3000)
+    }
+
+    useEffect(() => {
+        if (createLeadsData?.id) {
+            setCreateMessage({
+                msg: 'Lead criado com sucesso',
+                type: 'success'
+            })
+            getLeads()
+            clearMessage()
+        } else if (createLeadsError) {
+            setCreateMessage({
+                msg: 'Não foi possível realizar a operação. Entre em contato com nosso suporte.',
+                type: 'error'
+            })
+        } else {
+            clearMessage()
+        }
+    }, [createLeadsData, createLeadsError])
 
     return (
         <>
             <Header />
-            <h1>Leads</h1>
+            <Container className="mb-2" maxWidth="lg">
+                <Grid container spacing={4}>
+                    <Grid item xs={12} sm={7}>
+                        <CardComponent className={leadsLoading ?
+                            'skeleton-loading skeleton-loading-mh-2'
+                            : ''}>
+                            {!leadsError && !leadsLoading && (
+                                <>
+                                    <StyledH2 className="mb-1">Meus leads</StyledH2>
+                                    {leadsData?.length ? (
+                                        <CustomTable
+                                            headers={['Nome', 'Email', 'Telefone', '']}
+                                            rows={leadsData.map((lead) => [
+                                                <StyledP>{lead.name}</StyledP>,
+                                                <StyledP>{lead.email}</StyledP>,
+                                                <StyledP>{lead.phone}</StyledP>,
+                                                <StyledButton className="borderless-alert" onClick={() => handleDelete(lead.id)} disabled={leadsDeleteLoading}>
+                                                    Excluir
+                                                </StyledButton>
+                                            ])}
+                                        />
+                                    ) : (
+                                        <StyledSpan>Sem leads cadastrados</StyledSpan>
+                                    )}
+                                </>
+                            )}
+                        </CardComponent>
+                    </Grid>
+                    <Grid item xs={12} sm={5}>
+                        <CardComponent>
+                            <StyledH2 className="mb-1">Cadastrar Leads</StyledH2>
+                            <FormComponent
+                                inputs={inputs.map((input, index) => ({
+                                    ...input,
+                                    type: input.type,
+                                    placeholder: input.placeholder,
+                                    value: formValues[index] || '',
+                                    onChange: (e: ChangeEvent<HTMLInputElement>) => handleChange(index, (e.target as HTMLInputElement).value)
+                                }))}
+                                buttons={[
+                                    {
+                                        className: 'primary',
+                                        disabled: !formValid || createLeadsLoading || leadsDeleteLoading,
+                                        type: 'submit',
+                                        onClick: handleSubmit,
+                                        children: 'Cadastrar lead',
+                                    }
+                                ]}
+                                message={createMessage}
+                            />
+                        </CardComponent>
+                    </Grid>
+                </Grid>
+            </Container>
         </>
     )
 }
