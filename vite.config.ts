5a851fd31974dcc51022f84108aaaabaf77bff84
wagner1067 <barra-wag@hotmail.com>
2024-12-31 12:37:01 -0300

    Feat: Adding unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
-    include:['@emotion/styled'],
+    include: ['@emotion/styled'],
   },
   resolve: {
     alias: {
