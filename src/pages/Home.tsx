26a5978f63730aab20687cbdbf8b73e6dffbd11c
wagner1067 <barra-wag@hotmail.com>
2025-01-16 12:12:44 -0300

    Feat: Adding title to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -71,7 +71,7 @@ function Home() {
                 >
                   {!highlightsLoading && highlightsData && (
                     <>
-                      <StyledH2 className="mb-1">
+                      <StyledH2 className="mb-1" id='total-sales-title'>
                         Total de vendas no mês
                       </StyledH2>
                       <StyledH3 className="mb-1" size={40} lineheight={40}>
